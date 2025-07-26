6f5d5a5cc3369d42d785f4c8e807b135e0992c3e
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2025-07-26 13:42:17 +0600

    Update covid_etl_dag.py

--- 3-weeks-plan/week2-streaming-airflow/airflow/dags/covid_etl_dag.py
+++ 3-weeks-plan/week2-streaming-airflow/airflow/dags/covid_etl_dag.py
@@ -5,22 +5,7 @@ from datetime import datetime, timedelta
 # Import extraction and loading functions from airflow/ directory
 from airflow.extract import extract_covid_data
 from airflow.load import load_covid_data
-
-# Keep transformation logic here
-def transform_covid_data(**kwargs):
-    ti = kwargs['ti']
-    data = ti.xcom_pull(key='covid_data', task_ids='extract_covid_data')
-    if not data:
-        raise ValueError("No data to transform")
-    transformed = {
-        'cases': data.get('cases'),
-        'deaths': data.get('deaths'),
-        'recovered': data.get('recovered'),
-        'updated': data.get('updated')
-    }
-    ti.xcom_push(key='transformed_covid_data', value=transformed)
-
-# Import the plotting function as before
+from airflow.transform import transform_covid_data
 from airflow.covid_plotter import plot_covid_data
 
 default_args = {
